e3100f17a0ca01bdedb6c39826a82dc1abd52cd0
mannar.hielal <adminwithu@gmail.com>
2020-06-09 13:43:32 +0200

    Create Vue Project

--- time2ask/urls.py
+++ time2ask/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from core.views import IndexTemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('users.urls')),  # make the homepage is the home in users app
     # path('accounts/', include('django.contrib.auth.urls')),  # should be hooked to registration/login.html
     path('api-auth/', include('rest_framework.urls')), # authenticating access to browsable api
+    # create a url based on regular expressions, here we need to direct
+    # every request to this index.html which we will mount our vue.js to.
+    re_path(r"^.*$", IndexTemplateView.as_view(), name="entry-point")
 ]
 
